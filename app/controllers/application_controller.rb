6d8c902c4dc70caa4150051cd7510bf7e285ebac
phaedraa <phaedra.a.r@gmail.com>
2017-03-12 17:09:30 -0700

    Process date data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,75 +2,86 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  
+  require 'Date'
+  require 'nokogiri'
+  require 'open-uri'
+  require 'URI'
+  
   def scrape_wikipedia
-    require 'nokogiri'
-    require 'open-uri'
-    doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/Portal:Current_events/January_2010#2010_January_2"))
-    #css_table = doc.at_css('table')
-    tables = doc.css('table.vevent')
-    
-    first_table = tables[0]
-
-    #title
-    title_header = first_table.css('span.summary')
-    title_text = title[0].text
-    #"Current events of January 1, 2010 (2010-01-01) (Friday)"
-    paren1_idx = title_text.index("(")
-    paren2_idx = title_text.index(")")
-
-    #date
-    date = title_text[paren1_idx + 1 .. paren2_idx - 1]
+    date = '2016-3-2'
+    #debugger
+    date = Date.parse(date)
+    month = Date::MONTHNAMES[date.mon]
+    year = date.year
+    day = date.day
+    date_url = "#{month}_#{year}" + "#" + "#{year}_#{month}_#{day}"
+    doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/Portal:Current_events/#{date_url}"))
+    div_id = "#{year}_#{month}_#{day < 10 ? '0' : ''}#{day}"
+    table = doc.xpath("//div[@id='#{div_id}']").first.next_element
+    #next_date = date.next
+    #next_div_id = "#{next_date.year}_#{Date::MONTHNAMES[next_date.month]}_#{next_date.day}"
+    #tables = doc.css('table.vevent')
+    #first_table = tables[0]
+    #title_header = first_table.css('span.summary')
+    #title_text = title_header[0].text
+    ##"Current events of January 1, 2010 (2010-01-01) (Friday)"
 
     #content
-    content = first_table.css('td.description')
+    content = table.css('td.description')
     events = content.css('li')
-    event_urls = events.map do |event|
-      event.css('a')[0]['href']
+
+    data_to_log = []
+    events.each do |event|
+      data_to_log.push(getEventData(event.css('a')[0]))
     end
 
-    #event_text += text[idx]
+    #Cash this locally
+    @event_data = Hash[date => data_to_log]
 
-    #rows = tables.css('tr')
-    #column_names = rows.shift.css('th').map(&:text)
-    #puts column_names
-    #@occurances = []
-    #text_all_rows = rows.map do |row|
-    #  row_name = row.css('th').text
-    #  row_values = row.css('td').map(&:text)
-    #  [row_name, *row_values]
-    #end
-
-    #doc.xpath("//tr").each do |x|
-    #   date = x.css("td")[0].text
-    #   date2 = x.css("td")[1].text
-    #   date3 = x.css("td")[2].text
-    #   @occurances << {:date => date, :date2 => date2, :date3=>date3}
-    #end
-    #puts @occurances
-    puts text_all_rows
-    render text: doc 
+    render text: data_to_log 
   end
 
-  def getEventData(url)
-    first_event = Nokogiri::HTML(open("https://en.wikipedia.org" + url))
-    
-    heading = first_event.css('h1.firstHeading').text
-    content = first_event.css('div.mw-body-content').css('div.mw-content-ltr')
-    image_url = content.css('table.infobox').css('a.image')[0]['href']
-
-    paragraphs = content.xpath('//p[count(preceding-sibling::h2) = 0 and count(following-sibling::div) > 0]')
+  def getEventData(anchor_image)
+    url = anchor_image['href']
+    title = anchor_image.text
     summary = ''
-    paragraphs.each do |paragraph|
-      summary = summary + paragraph.text
+    image_url = ''
+    wiki_url = "https://en.wikipedia.org#{url}"
+    begin
+      first_event = Nokogiri::HTML(open("https://en.wikipedia.org#{url}"))
+      content = first_event.css('div.mw-body-content').css('div.mw-content-ltr')
+      title = first_event.css('h1.firstHeading').text
+      image_box = content.css('table.infobox').css('a.image')
+      image_url = image_box.length < 1 ? '' : image_box[0]['href']
+      
+      paragraphs = content.xpath(
+        '//p[count(preceding-sibling::h2) = 0 and count(preceding-sibling::table) > 0 and count(following-sibling::div) > 0]'
+      )
+      paragraphs.each do |paragraph|
+        summary = summary + paragraph.text
+      end
+    rescue OpenURI::HTTPError => e
+      print "URI: #{url} no longer exists: " + e.to_s
+    rescue
+      print "Not an HTTPError"
     end
 
     summary_stripped = strip_citations(summary)
 
-    return {:summary => summary_stripped, :image_url => image_url, :title => heading}
+    return {
+      :wiki_url => wiki_url,
+      :title => title,
+      :summary => summary_stripped,
+      :image_url => image_url
+    }
   end
 
   def strip_citations(summary)
+    if summary.length == 0
+      return ''
+    end
+
     idx_to_strip = []
     pair = []
     idx = 0
